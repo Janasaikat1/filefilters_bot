24d4f68a7a9bbef9e4f42ce934ea1d7b1fe29eb6
Janasaikat1 <108621376+Janasaikat1@users.noreply.github.com>
2022-07-04 13:41:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /filefilters_bot
 fi
-cd /file_filters_bot
+cd /filefilters_bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
