7282bf14be31a824baf11e3af87a8914b7a32d9f
Janasaikat1 <108621376+Janasaikat1@users.noreply.github.com>
2022-07-04 13:55:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -377,7 +377,7 @@ async def delete_all_index(bot, message):
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
 async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
-    await message.answer('Piracy Is Crime')
+    await message.answer('Please wait Baby...')
     await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
 
